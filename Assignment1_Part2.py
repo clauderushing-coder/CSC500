74b02a9f923a11a19917601cb0727764cd62cc52
clauderushing-coder <claude.rushing@csuglobal.edu>
2025-08-17 07:56:54 -0400

    Add files via upload

---
+++ Assignment1_Part2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 15 09:30:50 2025
+
+@author: Claude Rushing
+"""
+
+num1 = float(input("\nPlease enter the first number: "))
+num2 = float(input("Please enter the second number: "))
+product = float(num1*num2)
+print("\nThe Product of the two  numbers is: ", product)
+quotient = float(num1/num2)
+print("\nThe quotient of the two numbers is: ", quotient)
